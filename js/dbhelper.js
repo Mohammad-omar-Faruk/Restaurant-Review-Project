0d76e734ea4dcfddeb9eceaca590420f4445d3aa
Mohammad omar Faruk <momfaruk@gmail.com>
2018-07-20 20:32:45 +0200

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -10,7 +10,7 @@ class DBHelper {
     static get DATABASE_URL() {
 
         //const port = 8000; // Change this to your server port
-        return `https://github.com/Mohammad-omar-Faruk/Restaurant-Review-Project//data/restaurants.json`;
+        return `https://github.com/Mohammad-omar-Faruk/Restaurant-Review-Project/data/restaurants.json`;
   }
 
     /**
