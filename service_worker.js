7fceffadd129f007c3a6fc1e714d947e78b81df8
Mohammad omar Faruk <momfaruk@gmail.com>
2018-07-20 20:02:06 +0200

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -36,10 +36,11 @@ self.addEventListener('install', event => {
 });
 
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', function (event) {
+	event.respondWith(
+		caches.match(event.request).then(function (response) {
+			if (response) return response;
+			return fetch(event.request);
+		})
+	);
 });
